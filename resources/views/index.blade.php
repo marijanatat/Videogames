1d8e49dd208e244842c704e1703b153bed106afa
Marijana Tatalovic <tatalovicmarijana@gmail.com>
2020-06-18 13:04:07 +0200

    index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,292 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mx-auto px-4 pb-16">
+    <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+        Popular games
+    </h2>
+    <div class="popular games text-sm grid grid-cols-6 gap-12 border-b border-gray-800">
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/doom.jpg " class="hover:opacity-75 transition ease-in-out duration-150"
+                        alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Doom new</a>
+            <div class="text-gray-400 mt-1 ">Playstation 4 </div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/avengers.jpg " class="hover:opacity-75 transition ease-in-out duration-150"
+                        alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Avengers new</a>
+            <div class="text-gray-400 mt-1 ">XBox,PS4 </div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/cyberpunk.jpg " class="hover:opacity-75 transition ease-in-out duration-150"
+                        alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Cyberpunk</a>
+            <div class="text-gray-400 mt-1 ">Playstation 4 </div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/doom.jpg " class="hover:opacity-75 transition ease-in-out duration-150"
+                        alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Doom new</a>
+            <div class="text-gray-400 mt-1 ">Playstation 4 </div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/doom.jpg " class="hover:opacity-75 transition ease-in-out duration-150"
+                        alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Doom new</a>
+            <div class="text-gray-400 mt-1 ">Playstation 4 </div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/doom.jpg " class="hover:opacity-75 transition ease-in-out duration-150"
+                        alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Doom new</a>
+            <div class="text-gray-400 mt-1 ">Playstation 4 </div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/doom.jpg " class="hover:opacity-75 transition ease-in-out duration-150"
+                        alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Doom new</a>
+            <div class="text-gray-400 mt-1 ">Playstation 4 </div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/animalcrossing.jpg "
+                        class="hover:opacity-75 transition ease-in-out duration-150" alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Animal new</a>
+            <div class="text-gray-400 mt-1 ">Playstation 4 </div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/animalcrossing.jpg "
+                        class="hover:opacity-75 transition ease-in-out duration-150" alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Animal new</a>
+            <div class="text-gray-400 mt-1 ">Playstation 4 </div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href=""><img src="/images/animalcrossing.jpg "
+                        class="hover:opacity-75 transition ease-in-out duration-150" alt="game cover"></a>
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                    style="right:-20px;bottom:-20px">
+                    <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                </div>
+            </div>
+            <a href="" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Animal new</a>
+            <div class="text-gray-400 mt-1 ">Playstation 4 </div>
+        </div>
+    </div>
+    <!--end popular games-->
+
+
+    <div class="flex my-10">
+        <div class="recently reviewed w-3/4 mr-32">
+           <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+               Recently reviewed
+           </h2>
+           <div class="recently review container space-y-12 mt-8">
+               <div class="game flex shadow-lg bg-gray-800 rounded-lg px-6 py-6">
+                    <div class="relative flex-none">
+                        <a href=""><img src="/images/doom.jpg " class=" w-48 hover:opacity-75 transition ease-in-out duration-150"
+                            alt="game cover"></a>
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                            style="right:-20px;bottom:-20px">
+                            <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                        </div>
+                 </div>
+                    <div class="ml-12">
+                        <a href="" class="block text-lg  font-semibold leading-tight hover:text-gray-400 mt-4">Doom new</a>
+                         <div class="text-gray-400 mt-1">PS4</div>
+                         <div class="mt-6 text-gray-400 text-base">Developed by id Software, DOOM Eternal is the direct sequel to DOOM (2016). Experience the ultimate combination of speed and power with the next leap in push-forward, first-person combat.
+
+                            As the DOOM Slayer, return to take your vengeance against the forces of Hell. Set to an all-new pulse pounding soundtrack composed by Mick Gordon, fight across dimensions as you slay new and classic demons with powerful new weapons and abilities.</div>
+                    </div>
+                </div>
+
+                <div class="game flex shadow-lg bg-gray-800 rounded-lg px-6 py-6">
+                    <div class="relative flex-none">
+                        <a href=""><img src="/images/animalcrossing.jpg " class=" w-48 hover:opacity-75 transition ease-in-out duration-150"
+                            alt="game cover"></a>
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                            style="right:-20px;bottom:-20px">
+                            <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                        </div>
+                 </div>
+                    <div class="ml-12">
+                        <a href="" class="block text-lg  font-semibold leading-tight hover:text-gray-400 mt-4">Animal new</a>
+                         <div class="text-gray-400 mt-1">Nintendo</div>
+                         <div class="mt-6 text-gray-400 text-base">Time passes in Animal Crossing in real-time, as the month, day, season, and time of day matches that of the player's. Holiday seasons come and go in Animal Crossing, just as they would in the real world. Experience the freedom to fish, decorate your house, go bug catching, garden, write letters, and even play original Nintendo NES games in this imaginative, and addictive game.</div>
+                    </div>
+                </div>
+
+                <div class="game flex shadow-lg bg-gray-800 rounded-lg px-6 py-6">
+                    <div class="relative flex-none">
+                        <a href=""><img src="/images/doom.jpg " class=" w-48 hover:opacity-75 transition ease-in-out duration-150"
+                            alt="game cover"></a>
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                            style="right:-20px;bottom:-20px">
+                            <div class="font-semibold text-xs  h-full flex justify-center items-center">80%</div>
+                        </div>
+                 </div>
+                    <div class="ml-12">
+                        <a href="" class="block text-lg  font-semibold leading-tight hover:text-gray-400 mt-4">Animal new</a>
+                         <div class="text-gray-400 mt-1">PS4</div>
+                         <div class="mt-6 text-gray-400 text-base">Developed by id Software, DOOM Eternal is the direct sequel to DOOM (2016). Experience the ultimate combination of speed and power with the next leap in push-forward, first-person combat.
+
+                            As the DOOM Slayer, return to take your vengeance against the forces of Hell. Set to an all-new pulse pounding soundtrack composed by Mick Gordon, fight across dimensions as you slay new and classic demons with powerful new weapons and abilities.</div>
+                    </div>
+                </div>
+               
+
+           </div>
+
+        </div>
+ 
+
+        <!-- most anticipated-->
+        <div class="most anticipated w-1/4">
+            <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+                Most anticipated
+            </h2>
+            <div class="most antic container space-y-10 mt-8">
+                <div class="game flex">
+                     <a href=""><img src="/images/doom.jpg " class=" w-16 hover:opacity-75 transition ease-in-out duration-150"
+                        alt="game cover"></a>
+                        <div class="ml-4">
+                            <a href="" class="hover:text-gray-300">Doom</a>
+                            <div class="text-gray-400 text-sm mt-1"> Sep 16,2020</div>
+                        </div>
+                </div>
+
+                <div class="game flex">
+                    <a href=""><img src="/images/doom.jpg " class=" w-16 hover:opacity-75 transition ease-in-out duration-150"
+                       alt="game cover"></a>
+                       <div class="ml-4">
+                           <a href="" class="hover:text-gray-300">Doom</a>
+                           <div class="text-gray-400 text-sm mt-1"> Sep 16,2020</div>
+                       </div>
+               </div>
+
+               <div class="game flex">
+                     <a href=""><img src="/images/doom.jpg " class=" w-16 hover:opacity-75 transition ease-in-out duration-150"
+                        alt="game cover"></a>
+                        <div class="ml-4">
+                            <a href="" class="hover:text-gray-300">Doom</a>
+                            <div class="text-gray-400 text-sm mt-1"> Sep 16,2020</div>
+                        </div>
+                </div>
+
+                <div class="game flex">
+                    <a href=""><img src="/images/doom.jpg " class=" w-16 hover:opacity-75 transition ease-in-out duration-150"
+                       alt="game cover"></a>
+                       <div class="ml-4">
+                           <a href="" class="hover:text-gray-300">Doom</a>
+                           <div class="text-gray-400 text-sm mt-1"> Sep 16,2020</div>
+                       </div>
+               </div>
+            </div>
+
+
+            <!--comming soon-->
+            <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-12">Coming Soon</h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#"><img src="/images/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150"></a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                    <div class="game flex">
+                        <a href="#"><img src="/images/avengers.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150"></a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Marvel's Avengers</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 3, 2020</div>
+                        </div>
+                    </div>
+                    <div class="game flex">
+                        <a href="#"><img src="/images/ghost.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150"></a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Ghost of Tsushima</a>
+                            <div class="text-gray-400 text-sm mt-1">July 17, 2020</div>
+                        </div>
+                    </div>
+                    <div class="game flex">
+                        <a href="#"><img src="/images/tlou2.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150"></a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">The Last of Us Part II</a>
+                            <div class="text-gray-400 text-sm mt-1">June 19, 2020</div>
+                        </div>
+                    </div>
+            </div>
+            <!--end of comming soon-->
+   
+
+        </div>
+
+    </div>
+
+</div>
+<!--end container-->
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Video games') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <!-- Styles -->
+
+</head>
+
+<body class="bg-gray-900 text-white">
+
+    <header class="border-b border-gray-900">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="#">
+                    <img src="/images/laracasts-logo.svg" alt="logo" class="w-32 flex-none">
+                </a>
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="" class="hover:text-gray-400 ">Reviews</a></li>
+                    <li><a href="" class="hover:text-gray-400 ">Coming soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center ">
+                <div class="relative">
+                    <input type="text"
+                        class="pl-8 bg-gray-800 text-sm rounded-full px-3 py-1 w-64 focus:outline-none focus:shadow-outline "
+                        placeholder="Search...">
+                    <div class="absolute top-0 flex items-center h-full ml-2">
+                        <svg class="fill-current text-gray-400 w-4 " viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+
+                </div>
+                <div class="ml-6">
+                    <a href=""><img src="/images/avatar.jpg" alt="avatar" class="rounded-full w-8"></img></a>
+                </div>
+
+            </div>
+        </nav>
+
+    </header>
+    <main class="py-8">
+        @yield('content')
+    </main>
+    
+    <footer class="border-t border-gray-800">
+        <div class="mx-auto px-4 py-6">
+            Powered by <a href="" class="underline hover:text-gray-400">IGDB API</a>
+        </div>
+    </footer>
+
+</body>
+
+</html>
